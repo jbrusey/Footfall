7de8607e8183a1ab732dc25fbb652c60b3cf9f0e
Tomasz Prabucki <tom.prabucki@gmail.com>
2018-07-12 15:04:16 +0100

    LED support + concurrent CSV saving

--- Footfall/src/ofApp.cpp
+++ Footfall/src/ofApp.cpp
@@ -12,20 +12,20 @@ void ofApp::setup()
 {
 	ofSetVerticalSync(true);
 	ofSetFrameRate(25);
-	
+
 	cout << "-------------------- Footfall --------------------" << endl;
-	
+
 	configManager.loadConfiguration("config.json");
-	
+
 	_logToCsv = configManager.getConfiguration().useCsvLogging;
 	_logToServer = configManager.getConfiguration().useHttp;
-	
+
 	cameraManager.setup(configManager.getConfiguration().cameraConfig);
 	trackingManager.setup(configManager.getConfiguration().trackingConfig);
-	
+
 	if (_logToServer) httpManager.setup(configManager.getConfiguration().httpConfig);
 	if (_logToCsv) csvManager.setup("csvlogs");
-	
+
 	ofAddListener(trackingManager.blobIn, this, &ofApp::blobIn);
 	ofAddListener(trackingManager.blobOut, this, &ofApp::blobOut);
 }
@@ -34,7 +34,7 @@ void ofApp::exit()
 {
 	if (_logToServer) httpManager.close();
 	if (_logToCsv) csvManager.close();
-		
+
 	ofRemoveListener(trackingManager.blobIn, this, &ofApp::blobIn);
 	ofRemoveListener(trackingManager.blobOut, this, &ofApp::blobOut);
 }
@@ -52,7 +52,7 @@ void ofApp::draw()
 	cameraManager.draw();
 	trackingManager.draw();
 	ofPopMatrix();
-	
+
 	stringstream ss;
 	ss << "Footfall" << endl;
 	ss << "People In: " << peopleIn;
@@ -74,18 +74,24 @@ void ofApp::keyReleased(int key)
 //--------------------------------------------------------------
 void ofApp::blobIn(int &val)
 {
+	system("echo 1 >/sys/class/leds/led0/brightness");
 	peopleIn += val;
 	cout << val << " Blob(s) Came In" << endl;
-	
+
 	if (_logToServer) httpManager.post(ofToString(val));
 	if (_logToCsv) csvManager.addRecord(ofToString(val), ofGetTimestampString("%Y-%m-%d %H:%M:%S"));
+	if (_logToCsv) csvManager.close();
+	system("echo 0 >/sys/class/leds/led0/brightness");
 }
 //--------------------------------------------------------------
 void ofApp::blobOut(int &val)
 {
+	system("echo 1 >/sys/class/leds/led0/brightness");
 	peopleOut += abs(val);
 	cout << val << " Blob(s) Went Out" << endl;
-	
+
 	if (_logToServer) httpManager.post(ofToString(val));
 	if (_logToCsv) csvManager.addRecord(ofToString(val), ofGetTimestampString("%Y-%m-%d %H:%M:%S"));
+	if (_logToCsv) csvManager.close();
+	system("echo 0 >/sys/class/leds/led0/brightness");
 }
