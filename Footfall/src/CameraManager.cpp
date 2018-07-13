da522e6a0382c9218098ca844ddec59d2d299962
Tomasz Prabucki <tom.prabucki@gmail.com>
2018-07-13 14:44:03 +0100

    Increase camera size + point to correct test video

--- Footfall/src/CameraManager.cpp
+++ Footfall/src/CameraManager.cpp
@@ -10,7 +10,7 @@ void CameraManager::setup(Camera_Configuration _cameraConfig)
 {
 	cout << "Setting Up Camera Manager";
 	_useMask = _cameraConfig.bUseMask;
-	
+
 	// Check whether the mask should be generated
 	if (_useMask)
 	{
@@ -20,20 +20,20 @@ void CameraManager::setup(Camera_Configuration _cameraConfig)
 			cv::Point p = cv::Point(_cameraConfig.maskCoord[i].x,_cameraConfig.maskCoord[i].y);
 			_maskPts.push_back(p);
 		}
-		
+
 		mask = cvCreateMat(_cameraConfig.cameraheight, _cameraConfig.camerawidth, CV_8UC1);
 		combinedMask = cvCreateMat(_cameraConfig.cameraheight, _cameraConfig.camerawidth, CV_8UC1);
-		
+
 		// Fill the Mat with black
 		for(int i=0; i<mask.cols; i++)
 			for(int j=0; j<mask.rows; j++)
 				mask.at<uchar>(cv::Point(i,j)) = 0;
-		
+
 		vector<cv::Point> polyright;
 		approxPolyDP(_maskPts, polyright, 1.0, true);
 		fillConvexPoly(mask,&polyright[0],polyright.size(),255,8,0);
 	}
-	
+
 	// Setup the Background MOG2
 	pMOG2 = new BackgroundSubtractorMOG2(_cameraConfig.history,
 										 _cameraConfig.mogThreshold,
@@ -41,26 +41,26 @@ void CameraManager::setup(Camera_Configuration _cameraConfig)
 										 );
 	// This is the ratio of the shadow detection. I.e how many times lighter the shadow needs to be to be considered a blob.
 	pMOG2->setDouble("fTau", _cameraConfig.shadowPixelRatio);
-	
+
 #ifdef USE_VIDEO
 	cout << " - Using Video" << endl;
-	videoPlayer.load("yourtestfootage.mov");
+	videoPlayer.load("output.mp4");
 	videoPlayer.setLoopState(OF_LOOP_NORMAL);
 	videoPlayer.play();
 #endif
-	
+
 #ifdef USE_WEBCAM
 	cout << " - Using Web Camera" << endl;
 	videoGrabber.setVerbose(true);
 	videoGrabber.setup(_cameraConfig.camerawidth, _cameraConfig.cameraheight);
 #endif
-	
+
 #ifdef USE_PI_CAM
 	cout << " - Using Pi Camera" << endl;
 	piCamera.setup(_cameraConfig.camerawidth,_cameraConfig.cameraheight,true);
 	piCamera.setFlips(_cameraConfig.bFlipH,_cameraConfig.bFlipV);
 #endif
-	
+
 	_threshold = _cameraConfig.threshold;
 	_showShadows = _cameraConfig.bShowShadowImage;
 	_dilateAmount = _cameraConfig.dilateAmount;
@@ -72,7 +72,7 @@ void CameraManager::update()
 {
 #ifdef USE_VIDEO
 	videoPlayer.update();
-	
+
 	if (videoPlayer.isFrameNew())
 	{
 		copy(videoPlayer, videoMatrix);
@@ -81,7 +81,7 @@ void CameraManager::update()
 
 #ifdef USE_WEBCAM
 	videoGrabber.update();
-	
+
 	if (videoGrabber.isFrameNew())
 	{
 		copy(videoGrabber, videoMatrix);
@@ -91,12 +91,12 @@ void CameraManager::update()
 #ifdef USE_PI_CAM
 	videoMatrix = piCamera.grab();
 #endif
-	
+
 	if (!videoMatrix.empty())
 	{
 		// Blur the original image
 		GaussianBlur(videoMatrix, _blur);
-		
+
 		// Process the MOG
 		if (_useMask)
 		{
@@ -107,15 +107,15 @@ void CameraManager::update()
 		{
 			pMOG2->operator()(videoMatrix,processedMog);
 		}
-		
+
 		// Get the background image
 		pMOG2->getBackgroundImage(background);
-		
+
 		// If you want to see the effect of the shadow ratio threshold. Set the showshadow config to true
 		// Copy the MOG's output before processing the mat
-		
+
 		if(_showShadows) copy(processedMog,unprocessed_MOG);
-		
+
 		// Image processing
 		threshold(processedMog,_threshold);
 		blur(processedMog,_blur);
@@ -123,7 +123,7 @@ void CameraManager::update()
 		erode(processedMog,_erodeAmount);
 		dilate(processedMog,_dilateAmount);
 		GaussianBlur(processedMog, _blur*2);
-		
+
 		// Leave these two
 		threshold(processedMog,50);
 		erode(processedMog,2);
@@ -138,7 +138,7 @@ void CameraManager::draw()
 	drawMat(processedMog, videoMatrix.cols, 0,videoMatrix.cols,videoMatrix.rows);
 	ofDrawBitmapStringHighlight("Input Image", (videoMatrix.cols/2)-45,videoMatrix.rows+20);
 	ofDrawBitmapStringHighlight("Processed Image", ((videoMatrix.cols/2)*3)-65,videoMatrix.rows+20);
-	
+
 	if(_showShadows)
 	{
 		drawMat(unprocessed_MOG, videoMatrix.cols*2, 0,videoMatrix.cols,videoMatrix.rows);
