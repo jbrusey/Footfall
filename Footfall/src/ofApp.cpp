d1d580aa44d88d64eca6a0c25b4fe5eb9af51c5d
Tomasz Prabucki <tom.prabucki@gmail.com>
2018-07-16 16:17:53 +0100

    minor fix

--- Footfall/src/ofApp.cpp
+++ Footfall/src/ofApp.cpp
@@ -23,7 +23,7 @@ void ofApp::setup()
 	cameraManager.setup(configManager.getConfiguration().cameraConfig);
 	trackingManager.setup(configManager.getConfiguration().trackingConfig);
 
-	if (_logToServer) mqttManager.setup(configManager.getConfiguration().MQTTConfig);
+	if (_logToServer) mqttManager.setup(configManager.getConfiguration().MqttConfig);
 	if (_logToCsv) csvManager.setup("csvlogs");
 
 	ofAddListener(trackingManager.blobIn, this, &ofApp::blobIn);
