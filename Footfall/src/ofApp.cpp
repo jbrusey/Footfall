c3ec2848fe036e614a1911f39be89bcf23a76fb6
Tomasz Prabucki <tom.prabucki@gmail.com>
2018-07-19 16:32:37 +0100

    fix

--- Footfall/src/ofApp.cpp
+++ Footfall/src/ofApp.cpp
@@ -72,7 +72,7 @@ void ofApp::draw()
 	ofDrawBitmapStringHighlight(ss.str(),650, 20);
 
 	ofScale(0.5,0.5);
-	ofDrawBitmapStringHighlight(_csv_history.str(),1300, 200);
+	ofDrawBitmapStringHighlight(_csv_history.str(),1300, 100);
 }
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key)
