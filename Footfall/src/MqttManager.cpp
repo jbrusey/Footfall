ca3fc59a3053dee8780018e6118d76d7d01a7322
Tomasz Prabucki <tom.prabucki@gmail.com>
2018-07-19 14:15:19 +0100

    fix

--- Footfall/src/MqttManager.cpp
+++ Footfall/src/MqttManager.cpp
@@ -33,7 +33,7 @@ void MqttManager::connect(int attempts)
 
 void MqttManager::publish(std::string message)
 {
-	MQTT.connect(_id, _username, _password);
+	//MQTT.connect(_id, _username, _password);
 	MQTT.update();
   MQTT.publish(_MQTTTopic, message, _QoS, false);
   MQTT.update();
