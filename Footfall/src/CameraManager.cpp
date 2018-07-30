bc05a206ff1a09dc1b19fd074d87bceb0771a696
Tomasz Prabucki <tom.prabucki@gmail.com>
2018-07-30 13:32:49 +0100

    fix

--- Footfall/src/CameraManager.cpp
+++ Footfall/src/CameraManager.cpp
@@ -165,7 +165,7 @@ Mat CameraManager::getImage()
 	return processedMog;
 }
 
-void placeScreen(Mat screen, int x=0, int y=0, bool full=false, bool border=true, const std::string& caption = std::string())
+void placeScreen(Mat screen, int x=0, int y=0, bool full=false, bool border=true, const std::string& caption)
 {
 	if (full)
 		drawMat(screen, 0, 0, screen.cols*2, screen.rows*2);
