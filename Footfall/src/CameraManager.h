54a87c22fcd90d7d5416a86758d9e4b1510ff2dc
Tomasz Prabucki <tom.prabucki@gmail.com>
2018-07-30 13:29:32 +0100

    fix

--- Footfall/src/CameraManager.h
+++ Footfall/src/CameraManager.h
@@ -35,7 +35,7 @@ class CameraManager
 		//! Get Image
 		Mat getImage();
 
-		void placeScreen(Mat screen, int x=0, int y=0, bool full=false, bool border=true, std::string caption= std::string());
+		void placeScreen(Mat screen, int x=0, int y=0, bool full=false, bool border=true, const std::string& caption= std::string());
 
 	private:
 //#ifdef USE_WEBCAM
