7c6177c71b078f634291e5c4997723ac0c9ea9d9
Tomasz Prabucki <tom.prabucki@gmail.com>
2018-08-02 10:38:35 +0100

    fix

--- Footfall/src/CameraManager.cpp
+++ Footfall/src/CameraManager.cpp
@@ -146,10 +146,10 @@ void CameraManager::draw()
 	//ofDrawRectangle(0, videoMatrix.rows+29,videoMatrix.cols+2,videoMatrix.rows+2);
 
 	//drawMat(videoMatrix, 0, 0,videoMatrix.cols,videoMatrix.rows);
-	placeScreen(videoMatrix, "Input Image", 0, 0, false, true);
-	placeScreen(background, "Background", 1, 0, false, true);
-	placeScreen(unprocessed_MOG, "Shadows Image", 0, 1, false, true);
-	placeScreen(processedMog, "Processed Image", 1, 1, false, true);
+	//placeScreen(videoMatrix, "Input Image", 0, 0, false, true);
+	//placeScreen(background, "Background", 1, 0, false, true);
+	placeScreen(unprocessed_MOG, "Shadows Image", 0, 1, true, true);
+	//placeScreen(processedMog, "Processed Image", 1, 1, false, true);
 	//drawMat(background, videoMatrix.cols, 0,videoMatrix.cols,videoMatrix.rows);
 	//ofDrawBitmapStringHighlight("Input Image", (videoMatrix.cols/2)-45,videoMatrix.rows+20);
 	//ofDrawBitmapStringHighlight("Background", ((videoMatrix.cols/2)*3)-65,videoMatrix.rows+20);
